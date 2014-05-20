4c50397421ba4fd90e09fe809894c114b3d99f02
zkronenb <zkronenb@browning.genetics.utah.edu>
2014-05-20 12:03:31 -0600

    not sure if iHS was added

--- src/iHS.cpp
+++ src/iHS.cpp
@@ -18,6 +18,7 @@ using namespace vcf;
 void printVersion(void){
 	    cerr << "INFO: version 1.1.0 ; date: April 2014 ; author: Zev Kronenberg; email : zev.kronenberg@utah.edu " << endl;
 	    exit(1);
+
 }
 
 void printHelp(void){
